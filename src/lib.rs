2f4e6339d0a8f240b9b3133e04380762b2211a53
Simon Haegler <shaegler@esri.com>
2023-01-08 11:14:55 +0100

    cleanup: sort entities by importance

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,3 @@
-mod helpers {
-    use std::{ffi, fs, path};
-
-    pub(crate) fn from_char_ptr_to_string(cchar_ptr: *const ffi::c_char) -> String {
-        let val_cstr = unsafe { ffi::CStr::from_ptr(cchar_ptr) };
-        return val_cstr.to_str().unwrap_or_default().to_string();
-    }
-
-    #[allow(dead_code)]
-    #[cfg(target_os = "linux")]
-    fn wchar_is_utf32() -> bool {
-        true
-    }
-
-    #[allow(dead_code)]
-    #[cfg(target_os = "windows")]
-    fn wchar_is_utf32() -> bool {
-        false
-    }
-
-    #[allow(dead_code)]
-    pub fn from_wchar_ptr_to_string(ptr: *const libc::wchar_t) -> String {
-        assert!(!ptr.is_null());
-        let ptr_len = unsafe { libc::wcslen(ptr) };
-        assert!(ptr_len > 0);
-        return if wchar_is_utf32() {
-            let widestring_result = unsafe { widestring::U32CString::from_ptr(ptr as *const u32, ptr_len) };
-            let cstring = widestring_result.expect("could not convert wchar_t array to UTF32 string");
-            cstring.to_string().expect("could not convert to Rust string")
-        } else {
-            let widestring_result = unsafe { widestring::U16CString::from_ptr(ptr as *const u16, ptr_len) };
-            let cstring = widestring_result.expect("could not convert wchar_t array to UTF16 string");
-            cstring.to_string().expect("could not convert to Rust string")
-        };
-    }
-
-    pub fn from_string_to_wchar_vec(msg: &str) -> Vec<libc::wchar_t> {
-        return if cfg!(linux) {
-            let wide_msg = widestring::U32CString::from_str(msg)
-                .expect("cannot convert to UTF-32/wchar_t");
-            wide_msg.into_vec_with_nul().iter().map(|&e| e as libc::wchar_t).collect()
-        } else {
-            let wide_msg = widestring::U16CString::from_str(msg)
-                .expect("cannot convert to UTF-16/wchar_t");
-            wide_msg.into_vec_with_nul().iter().map(|&e| e as libc::wchar_t).collect()
-        };
-    }
-
-    // TODO: deduplicate with get_dependencies_path in build.rs
-    pub fn get_cesdk_path() -> path::PathBuf {
-        let out_dir = env!("OUT_DIR");
-        let crate_root = path::PathBuf::from(out_dir);
-        let deps_path = crate_root.join("prust_custom_deps");
-        let cesdk_path_entry = fs::read_dir(deps_path)
-            .expect("cannot read deps dir")
-            .filter(|p| p.is_ok() && p.as_ref().unwrap().file_type().unwrap().is_dir())
-            .find(|p| p.as_ref().unwrap()
-                .path().file_name().unwrap()
-                .to_str().unwrap().starts_with("esri_ce_sdk-"));
-        return cesdk_path_entry.expect("could not find cesdk lib dir").unwrap().path();
-    }
-}
-
 pub mod prt {
     use std::{collections, path};
     use std::ffi;
@@ -68,197 +5,10 @@ pub mod prt {
     use std::ptr;
     use std::ptr::{null, null_mut};
 
-    #[allow(non_camel_case_types)]
-    #[allow(dead_code)]
-    #[derive(PartialEq)]
-    #[derive(Debug)]
-    #[repr(C)]
-    pub enum Status {
-        STATUS_OK,
-        STATUS_UNSPECIFIED_ERROR,
-        STATUS_OUT_OF_MEM,
-        STATUS_NO_LICENSE,
-
-        STATUS_NOT_ALL_IS_GENERATED,
-        STATUS_INCOMPATIBLE_IS,
-
-        STATUS_FILE_NOT_FOUND,
-        STATUS_FILE_ALREADY_EXISTS,
-        STATUS_COULD_NOT_OPEN_FILE,
-        STATUS_COULD_NOT_CLOSE_FILE,
-        STATUS_FILE_WRITE_FAILED,
-        STATUS_FILE_READ_FAILED,
-        STATUS_FILE_SEEK_FAILED,
-        STATUS_FILE_TELL_FAILED,
-        STATUS_NO_SEEK,
-        STATUS_EMPTY_FILE,
-        STATUS_INVALID_URI,
-
-        STATUS_STREAM_ADAPTOR_NOT_FOUND,
-        STATUS_RESOLVEMAP_PROVIDER_NOT_FOUND,
-        STATUS_DECODER_NOT_FOUND,
-        STATUS_ENCODER_NOT_FOUND,
-        STATUS_UNABLE_TO_RESOLVE,
-        STATUS_CHECK_ERROR_PARAM,
-        STATUS_KEY_NOT_FOUND,
-        STATUS_KEY_ALREADY_TAKEN,
-        STATUS_KEY_NOT_SUPPORTED,
-        STATUS_STRING_TRUNCATED,
-        STATUS_ILLEGAL_CALLBACK_OBJECT,
-        STATUS_ILLEGAL_LOG_HANDLER,
-        STATUS_ILLEGAL_LOG_LEVEL,
-        STATUS_ILLEGAL_VALUE,
-        STATUS_NO_RULEFILE,
-        STATUS_NO_INITIAL_SHAPE,
-        STATUS_CGB_ERROR,
-        STATUS_NOT_INITIALIZED,
-        STATUS_ALREADY_INITIALIZED,
-        STATUS_INCONSISTENT_TEXTURE_PARAMS,
-        STATUS_CANCELED,
-        STATUS_UNKNOWN_ATTRIBUTE,
-        STATUS_UNKNOWN_RULE,
-        STATUS_ARGUMENTS_MISMATCH,
-        STATUS_BUFFER_TO_SMALL,
-        STATUS_UNKNOWN_FORMAT,
-        STATUS_ENCODE_FAILED,
-        STATUS_ATTRIBUTES_ALREADY_SET,
-        STATUS_ATTRIBUTES_NOT_SET,
-        STATUS_GEOMETRY_ALREADY_SET,
-        STATUS_GEOMETRY_NOT_SET,
-        STATUS_ILLEGAL_GEOMETRY,
-        STATUS_NO_GEOMETRY,
-    }
-
-    #[allow(non_camel_case_types)]
-    #[allow(dead_code)]
-    #[repr(C)]
-    pub enum LogLevel {
-        LOG_TRACE = 0,
-        LOG_DEBUG = 1,
-        LOG_INFO = 2,
-        LOG_WARNING = 3,
-        LOG_ERROR = 4,
-        LOG_FATAL = 5,
-        LOG_NO = 1000,
-    }
-
     #[derive(Debug)]
     pub struct PrtError {
         pub message: String,
         pub status: Option<Status>,
-    }
-
-    #[repr(C)]
-    struct Version {
-        version_major: i32,
-        version_minor: i32,
-        version_build: i32,
-
-        name: *const ffi::c_char,
-        full_name: *const ffi::c_char,
-        version: *const ffi::c_char,
-        build_config: *const ffi::c_char,
-        build_os: *const ffi::c_char,
-        build_arch: *const ffi::c_char,
-        build_tc: *const ffi::c_char,
-        build_date: *const ffi::c_char,
-
-        name_w: *const libc::wchar_t,
-        full_name_w: *const libc::wchar_t,
-        version_w: *const libc::wchar_t,
-        build_config_w: *const libc::wchar_t,
-        build_os_w: *const libc::wchar_t,
-        build_arch_w: *const libc::wchar_t,
-        build_tc_w: *const libc::wchar_t,
-        build_date_w: *const libc::wchar_t,
-
-        cga_version_major: i32,
-        cga_version_minor: i32,
-        cga_version: *const ffi::c_char,
-        cga_version_w: *const i32,
-
-        cgac_version_major: i32,
-        cgac_version_minor: i32,
-        cgac_version: *const ffi::c_char,
-        cgac_version_w: *const i32,
-    }
-
-    extern "C" {
-        #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-        #[link_name = "\u{1}_ZN3prt10getVersionEv"]
-        fn ffi_get_version() -> *const Version;
-    }
-
-    pub struct PrtVersion {
-        pub version_major: i32,
-        pub version_minor: i32,
-        pub version_build: i32,
-        pub version_string: String,
-
-        pub name: String,
-        pub full_name: String,
-
-        pub build_config: String,
-        pub build_os: String,
-        pub build_arch: String,
-        pub build_tc: String,
-        pub build_date: String,
-
-        pub cga_version_major: i32,
-        pub cga_version_minor: i32,
-        pub cga_version_string: String,
-
-        pub cgac_version_major: i32,
-        pub cgac_version_minor: i32,
-        pub cgac_version_string: String,
-    }
-
-    pub fn get_version() -> Result<PrtVersion, PrtError> {
-        unsafe {
-            let version_ptr = ffi_get_version();
-            if version_ptr == ptr::null() {
-                return Err(PrtError {
-                    message: "Could not get PRT version info".to_string(),
-                    status: None,
-                });
-            }
-            let version_ref = &*version_ptr;
-            let ver = PrtVersion {
-                version_major: version_ref.version_major,
-                version_minor: version_ref.version_minor,
-                version_build: version_ref.version_build,
-                version_string: crate::helpers::from_char_ptr_to_string(version_ref.version),
-                name: crate::helpers::from_char_ptr_to_string(version_ref.name),
-                full_name: crate::helpers::from_char_ptr_to_string(version_ref.full_name),
-                build_config: crate::helpers::from_char_ptr_to_string(version_ref.build_config),
-                build_os: crate::helpers::from_char_ptr_to_string(version_ref.build_os),
-                build_arch: crate::helpers::from_char_ptr_to_string(version_ref.build_arch),
-                build_tc: crate::helpers::from_char_ptr_to_string(version_ref.build_tc),
-                build_date: crate::helpers::from_char_ptr_to_string(version_ref.build_date),
-                cga_version_major: version_ref.cga_version_major,
-                cga_version_minor: version_ref.cga_version_minor,
-                cga_version_string: crate::helpers::from_char_ptr_to_string(version_ref.cga_version),
-                cgac_version_major: version_ref.cgac_version_major,
-                cgac_version_minor: version_ref.cgac_version_minor,
-                cgac_version_string: crate::helpers::from_char_ptr_to_string(version_ref.cgac_version),
-            };
-            Ok(ver)
-        }
-    }
-
-    extern "C" {
-        #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-        #[link_name = "\u{1}_ZN3prt20getStatusDescriptionENS_6StatusE"]
-        fn ffi_get_status_description(input: Status) -> *const ffi::c_char;
-    }
-
-    pub fn get_status_description(status: Status) -> String {
-        unsafe {
-            let status_description_cchar_ptr = ffi_get_status_description(status);
-            let status_description_cstr = ffi::CStr::from_ptr(status_description_cchar_ptr);
-            let status_description = status_description_cstr.to_str().unwrap_or_default();
-            return String::from(status_description);
-        }
     }
 
     #[repr(C)]
@@ -408,7 +158,7 @@ pub mod prt {
                 name: self.name.as_ptr(),
                 attributes: null(), // TODO
                 resolve_map: null(), // TODO
-            }
+            };
         }
     }
 
@@ -499,6 +249,19 @@ pub mod prt {
         }
     }
 
+    #[allow(non_camel_case_types)]
+    #[allow(dead_code)]
+    #[repr(C)]
+    pub enum LogLevel {
+        LOG_TRACE = 0,
+        LOG_DEBUG = 1,
+        LOG_INFO = 2,
+        LOG_WARNING = 3,
+        LOG_ERROR = 4,
+        LOG_FATAL = 5,
+        LOG_NO = 1000,
+    }
+
     pub trait LogHandler {
         fn handle_log_event(&mut self, msg: &str);
     }
@@ -576,6 +339,180 @@ pub mod prt {
         }
     }
 
+    #[allow(non_camel_case_types)]
+    #[allow(dead_code)]
+    #[derive(PartialEq)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub enum Status {
+        STATUS_OK,
+        STATUS_UNSPECIFIED_ERROR,
+        STATUS_OUT_OF_MEM,
+        STATUS_NO_LICENSE,
+
+        STATUS_NOT_ALL_IS_GENERATED,
+        STATUS_INCOMPATIBLE_IS,
+
+        STATUS_FILE_NOT_FOUND,
+        STATUS_FILE_ALREADY_EXISTS,
+        STATUS_COULD_NOT_OPEN_FILE,
+        STATUS_COULD_NOT_CLOSE_FILE,
+        STATUS_FILE_WRITE_FAILED,
+        STATUS_FILE_READ_FAILED,
+        STATUS_FILE_SEEK_FAILED,
+        STATUS_FILE_TELL_FAILED,
+        STATUS_NO_SEEK,
+        STATUS_EMPTY_FILE,
+        STATUS_INVALID_URI,
+
+        STATUS_STREAM_ADAPTOR_NOT_FOUND,
+        STATUS_RESOLVEMAP_PROVIDER_NOT_FOUND,
+        STATUS_DECODER_NOT_FOUND,
+        STATUS_ENCODER_NOT_FOUND,
+        STATUS_UNABLE_TO_RESOLVE,
+        STATUS_CHECK_ERROR_PARAM,
+        STATUS_KEY_NOT_FOUND,
+        STATUS_KEY_ALREADY_TAKEN,
+        STATUS_KEY_NOT_SUPPORTED,
+        STATUS_STRING_TRUNCATED,
+        STATUS_ILLEGAL_CALLBACK_OBJECT,
+        STATUS_ILLEGAL_LOG_HANDLER,
+        STATUS_ILLEGAL_LOG_LEVEL,
+        STATUS_ILLEGAL_VALUE,
+        STATUS_NO_RULEFILE,
+        STATUS_NO_INITIAL_SHAPE,
+        STATUS_CGB_ERROR,
+        STATUS_NOT_INITIALIZED,
+        STATUS_ALREADY_INITIALIZED,
+        STATUS_INCONSISTENT_TEXTURE_PARAMS,
+        STATUS_CANCELED,
+        STATUS_UNKNOWN_ATTRIBUTE,
+        STATUS_UNKNOWN_RULE,
+        STATUS_ARGUMENTS_MISMATCH,
+        STATUS_BUFFER_TO_SMALL,
+        STATUS_UNKNOWN_FORMAT,
+        STATUS_ENCODE_FAILED,
+        STATUS_ATTRIBUTES_ALREADY_SET,
+        STATUS_ATTRIBUTES_NOT_SET,
+        STATUS_GEOMETRY_ALREADY_SET,
+        STATUS_GEOMETRY_NOT_SET,
+        STATUS_ILLEGAL_GEOMETRY,
+        STATUS_NO_GEOMETRY,
+    }
+
+    extern "C" {
+        #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+        #[link_name = "\u{1}_ZN3prt20getStatusDescriptionENS_6StatusE"]
+        fn ffi_get_status_description(input: Status) -> *const ffi::c_char;
+    }
+
+    pub fn get_status_description(status: Status) -> String {
+        unsafe {
+            let status_description_cchar_ptr = ffi_get_status_description(status);
+            let status_description_cstr = ffi::CStr::from_ptr(status_description_cchar_ptr);
+            let status_description = status_description_cstr.to_str().unwrap_or_default();
+            return String::from(status_description);
+        }
+    }
+
+    #[repr(C)]
+    struct Version {
+        version_major: i32,
+        version_minor: i32,
+        version_build: i32,
+
+        name: *const ffi::c_char,
+        full_name: *const ffi::c_char,
+        version: *const ffi::c_char,
+        build_config: *const ffi::c_char,
+        build_os: *const ffi::c_char,
+        build_arch: *const ffi::c_char,
+        build_tc: *const ffi::c_char,
+        build_date: *const ffi::c_char,
+
+        name_w: *const libc::wchar_t,
+        full_name_w: *const libc::wchar_t,
+        version_w: *const libc::wchar_t,
+        build_config_w: *const libc::wchar_t,
+        build_os_w: *const libc::wchar_t,
+        build_arch_w: *const libc::wchar_t,
+        build_tc_w: *const libc::wchar_t,
+        build_date_w: *const libc::wchar_t,
+
+        cga_version_major: i32,
+        cga_version_minor: i32,
+        cga_version: *const ffi::c_char,
+        cga_version_w: *const i32,
+
+        cgac_version_major: i32,
+        cgac_version_minor: i32,
+        cgac_version: *const ffi::c_char,
+        cgac_version_w: *const i32,
+    }
+
+    extern "C" {
+        #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+        #[link_name = "\u{1}_ZN3prt10getVersionEv"]
+        fn ffi_get_version() -> *const Version;
+    }
+
+    pub struct PrtVersion {
+        pub version_major: i32,
+        pub version_minor: i32,
+        pub version_build: i32,
+        pub version_string: String,
+
+        pub name: String,
+        pub full_name: String,
+
+        pub build_config: String,
+        pub build_os: String,
+        pub build_arch: String,
+        pub build_tc: String,
+        pub build_date: String,
+
+        pub cga_version_major: i32,
+        pub cga_version_minor: i32,
+        pub cga_version_string: String,
+
+        pub cgac_version_major: i32,
+        pub cgac_version_minor: i32,
+        pub cgac_version_string: String,
+    }
+
+    pub fn get_version() -> Result<PrtVersion, PrtError> {
+        unsafe {
+            let version_ptr = ffi_get_version();
+            if version_ptr == ptr::null() {
+                return Err(PrtError {
+                    message: "Could not get PRT version info".to_string(),
+                    status: None,
+                });
+            }
+            let version_ref = &*version_ptr;
+            let ver = PrtVersion {
+                version_major: version_ref.version_major,
+                version_minor: version_ref.version_minor,
+                version_build: version_ref.version_build,
+                version_string: crate::helpers::from_char_ptr_to_string(version_ref.version),
+                name: crate::helpers::from_char_ptr_to_string(version_ref.name),
+                full_name: crate::helpers::from_char_ptr_to_string(version_ref.full_name),
+                build_config: crate::helpers::from_char_ptr_to_string(version_ref.build_config),
+                build_os: crate::helpers::from_char_ptr_to_string(version_ref.build_os),
+                build_arch: crate::helpers::from_char_ptr_to_string(version_ref.build_arch),
+                build_tc: crate::helpers::from_char_ptr_to_string(version_ref.build_tc),
+                build_date: crate::helpers::from_char_ptr_to_string(version_ref.build_date),
+                cga_version_major: version_ref.cga_version_major,
+                cga_version_minor: version_ref.cga_version_minor,
+                cga_version_string: crate::helpers::from_char_ptr_to_string(version_ref.cga_version),
+                cgac_version_major: version_ref.cgac_version_major,
+                cgac_version_minor: version_ref.cgac_version_minor,
+                cgac_version_string: crate::helpers::from_char_ptr_to_string(version_ref.cgac_version),
+            };
+            Ok(ver)
+        }
+    }
+
     #[cfg(test)]
     mod tests {
         use super::*;
@@ -636,5 +573,68 @@ pub mod prt {
             map.insert("foo".to_string(), PrimitiveType::String("bar".to_string()));
             assert_eq!(map.get("foo"), Some(&PrimitiveType::String("bar".to_string())));
         }
+    }
+}
+
+mod helpers {
+    use std::{ffi, fs, path};
+
+    pub(crate) fn from_char_ptr_to_string(cchar_ptr: *const ffi::c_char) -> String {
+        let val_cstr = unsafe { ffi::CStr::from_ptr(cchar_ptr) };
+        return val_cstr.to_str().unwrap_or_default().to_string();
+    }
+
+    #[allow(dead_code)]
+    #[cfg(target_os = "linux")]
+    fn wchar_is_utf32() -> bool {
+        true
+    }
+
+    #[allow(dead_code)]
+    #[cfg(target_os = "windows")]
+    fn wchar_is_utf32() -> bool {
+        false
+    }
+
+    #[allow(dead_code)]
+    pub fn from_wchar_ptr_to_string(ptr: *const libc::wchar_t) -> String {
+        assert!(!ptr.is_null());
+        let ptr_len = unsafe { libc::wcslen(ptr) };
+        assert!(ptr_len > 0);
+        return if wchar_is_utf32() {
+            let widestring_result = unsafe { widestring::U32CString::from_ptr(ptr as *const u32, ptr_len) };
+            let cstring = widestring_result.expect("could not convert wchar_t array to UTF32 string");
+            cstring.to_string().expect("could not convert to Rust string")
+        } else {
+            let widestring_result = unsafe { widestring::U16CString::from_ptr(ptr as *const u16, ptr_len) };
+            let cstring = widestring_result.expect("could not convert wchar_t array to UTF16 string");
+            cstring.to_string().expect("could not convert to Rust string")
+        };
+    }
+
+    pub fn from_string_to_wchar_vec(msg: &str) -> Vec<libc::wchar_t> {
+        return if cfg!(linux) {
+            let wide_msg = widestring::U32CString::from_str(msg)
+                .expect("cannot convert to UTF-32/wchar_t");
+            wide_msg.into_vec_with_nul().iter().map(|&e| e as libc::wchar_t).collect()
+        } else {
+            let wide_msg = widestring::U16CString::from_str(msg)
+                .expect("cannot convert to UTF-16/wchar_t");
+            wide_msg.into_vec_with_nul().iter().map(|&e| e as libc::wchar_t).collect()
+        };
+    }
+
+    // TODO: deduplicate with get_dependencies_path in build.rs
+    pub fn get_cesdk_path() -> path::PathBuf {
+        let out_dir = env!("OUT_DIR");
+        let crate_root = path::PathBuf::from(out_dir);
+        let deps_path = crate_root.join("prust_custom_deps");
+        let cesdk_path_entry = fs::read_dir(deps_path)
+            .expect("cannot read deps dir")
+            .filter(|p| p.is_ok() && p.as_ref().unwrap().file_type().unwrap().is_dir())
+            .find(|p| p.as_ref().unwrap()
+                .path().file_name().unwrap()
+                .to_str().unwrap().starts_with("esri_ce_sdk-"));
+        return cesdk_path_entry.expect("could not find cesdk lib dir").unwrap().path();
     }
 }
